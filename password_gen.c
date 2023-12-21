a5bad9b02c148dfc6ada40c7a62cb1be4b3b4a8f
Vinicius de Souza <vdsbt33@gmail.com>
2023-12-21 23:40:16 +0000

    Loop to generate multiple passwords.

--- password_gen.c
+++ password_gen.c
@@ -11,21 +11,28 @@ int random_number(int* min, int* max) {
     }
 }
 
+char random_number_char() {
+    int num_range[2] = {48, 58};
+    return random_number(&num_range[0], &num_range[1]);
+}
+
 char random_letter(int isUppercase) {
     if (isUppercase == 1)
     {
-        int lowercase_range[2] = {97, 122+1};
+        int lowercase_range[2] = {97, 123};
         return random_number(&lowercase_range[0], &lowercase_range[1]);
     }
     else
     {
-        int uppercase_range[2] = {65, 90+1};
+        int uppercase_range[2] = {65, 91};
         return random_number(&uppercase_range[0], &uppercase_range[1]);
     }
 }
 
 char random_symbol() {
-    return '.';
+    int symbols_range[2] = {0, 6};
+    int symbols_list[5] = {33, 36, 42, 46, 47};
+    return symbols_list[random_number(&symbols_range[0], &symbols_range[1])];
 }
 
 void clear_array(char* text, int size) {
@@ -35,9 +42,9 @@ void clear_array(char* text, int size) {
 }
 
 char* generate_password(int size) {
-    int boolean_range[2] = {0, 1+1};
-    int numbers_range[2] = {0, 9+1};
-    int lowercase_letters_range[2] = {0, 5};
+    int random_char_range[2] = {0, 4};
+    int boolean_range[2] = {0, 2};
+    int numbers_range[2] = {0, 10};
 
     char* password_result = malloc(sizeof(char) * (size + 1));
     clear_array(password_result, size);
@@ -46,7 +53,31 @@ char* generate_password(int size) {
 
     for (int i = 1; i < size; i++)
     {
-        password_result[i] = random_letter(random_number(&boolean_range[0], &boolean_range[1]));
+        int random_char = random_number(&random_char_range[0], &random_char_range[1]);
+
+        // Number
+        if (random_char == 0) {
+            password_result[i] = random_number_char();
+        }
+
+        // Lowercase letter
+        else if (random_char == 1) {
+            password_result[i] = random_letter(0);
+        }
+
+        // Uppercase letter
+        else if (random_char == 2) {
+            password_result[i] = random_letter(1);
+        }
+
+        // Symbol
+        else if (random_char == 3) {
+            password_result[i] = random_symbol();
+        }
+
+        else {
+            password_result[i] = '?';
+        }
     }
  
     return password_result;
@@ -68,10 +99,14 @@ int main() {
     puts("Generating password(s)...");
 
     srand(time(NULL));
-    char* password_generated = generate_password(20);
-    puts("Password generated:");
-    printf("%s", password_generated);
+
+    for (int i = 0; i < pass_amount; i++)
+    {
+        char* password_generated = generate_password(pass_size);
+        printf("%s\n", password_generated);
  
-    free(password_generated);
+        free(password_generated);
+    }
+    puts("\nPasswords generated.");
     return 0;
 }
